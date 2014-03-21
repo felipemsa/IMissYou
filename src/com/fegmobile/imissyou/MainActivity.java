788a03f784067f24d30b615f0f79384ca09a3364
Felipe Almeida <felipe.fulgur@gmail.com>
2014-03-21 01:13:30 -0300

    Inclusao de banner

--- src/com/fegmobile/imissyou/MainActivity.java
+++ src/com/fegmobile/imissyou/MainActivity.java
@@ -7,15 +7,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.fegmobile.imissyou.service.MediaPlayerService;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends Activity
 {
+	private static final String AD_UNIT_ID = "ca-app-pub-7746920603636507/5500421677";
 	RelativeLayout rlMain;
 	TextView tvPlay;
+	private AdView adView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -28,6 +34,18 @@ public class MainActivity extends Activity
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+	    adView = new AdView(this);
+	    adView.setAdSize(AdSize.BANNER);
+	    adView.setAdUnitId(AD_UNIT_ID);
+
+	    LinearLayout layout = (LinearLayout) findViewById(R.id.lnBan);
+	    layout.addView(adView);
+
+	    AdRequest adRequest = new AdRequest.Builder().build();
+	    
+	    adView.loadAd(adRequest);
+		
 		final Intent serviceIntent = new Intent(getApplicationContext(), MediaPlayerService.class);
 		
 		rlMain = (RelativeLayout) findViewById(R.id.rlMain);
@@ -52,6 +70,35 @@ public class MainActivity extends Activity
 		});
 		
 		return true;
+	}
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		if (adView != null) {
+			adView.resume();
+		}
+	}
+	
+	@Override
+	public void onPause()
+	{
+		if (adView != null) {
+			adView.pause();
+		}
+		super.onPause();
+	}
+	
+	/** Called before the activity is destroyed. */
+	@Override
+	public void onDestroy()
+	{
+		// Destroy the AdView.
+		if (adView != null) {
+			adView.destroy();
+		}
+		super.onDestroy();
 	}
 	
 }
